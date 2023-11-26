acd4117702b8d139f233d563cf7bb8222e72e473
mabrinasueller <mueller.sabrina@yahoo.de>
2023-11-26 18:23:16 +0100

    implement tailwind for styling, adjust naming and gitignore file

---
+++ user-search/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.html",
+    "./src/**/*.ts",
+    "./src/**/*.vue",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
