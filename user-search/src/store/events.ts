f72807534f7a6200ab5e05f2225f50c47b92d3bb
mabrinasueller <mueller.sabrina@yahoo.de>
2023-11-26 18:26:54 +0100

    add localStorage for keeping user list persistent on page refresh

--- user-search/src/store/events.ts
+++ user-search/src/store/events.ts
@@ -1,10 +1,34 @@
 import { defineStore } from "pinia";
 import { User } from "../types/types";
-import { fetchUsers } from "@/services/useApi";
 
 export const useMyStore = defineStore("users", {
   state: () => ({
     users: [] as User[],
+    selectedUser: null as User | null,
+    filters: {
+      gender: "all",
+      search: "",
+    },
+    loading: false,
   }),
-  actions: {},
+  actions: {
+    initializeStoreFromLocalStorage() {
+      const storedState = localStorage.getItem("appState");
+      if (storedState) {
+        const parsedState = JSON.parse(storedState);
+        this.users = parsedState.users || [];
+        this.selectedUser = parsedState.selectedUser || null;
+        this.filters = parsedState.filters || { gender: "all", search: "" };
+      }
+    },
+
+    saveStoreToLocalStorage() {
+      const stateToSave = {
+        users: this.users,
+        selectedUser: this.selectedUser,
+        filters: this.filters,
+      };
+      localStorage.setItem("appState", JSON.stringify(stateToSave));
+    },
+  },
 });
