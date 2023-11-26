258135581e51bcd9681e9cdfbc9218e0b60d0817
mabrinasueller <mueller.sabrina@yahoo.de>
2023-11-26 18:27:09 +0100

    add Infinite Scroll for user list

--- user-search/src/components/InfiniteScroll.vue
+++ user-search/src/components/InfiniteScroll.vue
@@ -1,1 +1,27 @@
-<script setup lang="ts"></script>
+<template>
+  <div ref="scrollContainer">
+    <slot></slot>
+    <div v-if="loading">Loading...</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, defineEmits } from "vue";
+import { useInfiniteScroll } from "@vueuse/core";
+import { useMyStore } from "@/store/events";
+import debounce from "lodash/debounce";
+
+const scrollContainer = ref(null);
+const loading = ref(false);
+const store = useMyStore();
+
+const emit = defineEmits(["loadMore"]);
+
+const debouncedLoadMore = debounce(async () => {
+  if (store.filters.gender !== "all" || store.filters.search !== "") return;
+  if (store.filters.gender !== "all" && store.filters.search !== "") return;
+  await emit("loadMore");
+}, 100);
+
+useInfiniteScroll(scrollContainer, debouncedLoadMore, { distance: 50 });
+</script>
